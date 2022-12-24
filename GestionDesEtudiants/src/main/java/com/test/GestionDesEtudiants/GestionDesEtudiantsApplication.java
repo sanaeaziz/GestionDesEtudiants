66a0f3c9fcde41f8bab2fa93a81665cab657af8b
Sanae Aziz <sanae.aziz0101@gmail.com>
2022-12-24 19:08:40 +0100

    add design to file html

--- GestionDesEtudiants/src/main/java/com/test/GestionDesEtudiants/GestionDesEtudiantsApplication.java
+++ GestionDesEtudiants/src/main/java/com/test/GestionDesEtudiants/GestionDesEtudiantsApplication.java
@@ -15,19 +15,24 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @SpringBootApplication
-public class GestionDesEtudiantsApplication {
+public class  GestionDesEtudiantsApplication {
 
 	public static void main(String[] args) throws ParseException {
 
 		ApplicationContext ctx =SpringApplication.run(GestionDesEtudiantsApplication.class, args);
+		/**
 		EtudiantRepository etudiantRepository = ctx.getBean(EtudiantRepository.class);
+
 		DateFormat df =new SimpleDateFormat("dd/mm/yyyy");
 		etudiantRepository.save(new Etudiant("sanae","v357606","sanae@gmail.com",df.parse("01/01/2001"),"sanae.jpg"));
 		etudiantRepository.save(new Etudiant("aziz","vb57606","aziz@gmail.com",df.parse("21/01/2000"),"aziz.jpg"));
+		etudiantRepository.save(new Etudiant("aya","b15265","aya@gmail.com",df.parse("09/08/1998"),"aya.jpg"));
+
 		etudiantRepository.save(new Etudiant("asmae","vpp57606","asmae@gmail.com",df.parse("01/03/1999"),"asmae.jpg"));
 
-	Page<Etudiant> etd = etudiantRepository.searchEtudiantsCNE("vpp57606",PageRequest.of(0, 4));
+	Page<Etudiant> etd = etudiantRepository.searchEtudiantsCNE("b15265",PageRequest.of(0, 4));
 	etd.forEach(e -> System.out.println(e.getName()));
+		 **/
 	}
 
 }
--- GestionDesEtudiants/src/main/java/com/test/GestionDesEtudiants/service/EtudiantController.java
+++ GestionDesEtudiants/src/main/java/com/test/GestionDesEtudiants/service/EtudiantController.java
@@ -2,6 +2,7 @@ package com.test.GestionDesEtudiants.service;
 
 import com.test.GestionDesEtudiants.models.Etudiant;
 import com.test.GestionDesEtudiants.repository.EtudiantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,18 +12,14 @@ import java.util.List;
 @Controller
 @RequestMapping(value = "/Etudiant")
 public class EtudiantController {
+    @Autowired
     private EtudiantRepository etudiantRepository;
 
-        @RequestMapping(value = "/error")
-        public String error() {
-            return "Error handling";
-        }
-
-
-    @RequestMapping(value="/index")
-    public String Index( Model model){
+    @RequestMapping(value="/Index")
+    public String Index(Model model){
         List<Etudiant> etds = etudiantRepository.findAll();
         model.addAttribute("etudiants", etds);
+
         return "etudiants";
     }
 
