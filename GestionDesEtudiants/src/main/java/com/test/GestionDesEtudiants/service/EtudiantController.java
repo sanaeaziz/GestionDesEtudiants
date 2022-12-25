74b13123437b3ae9e013d7f3659b4a6a40ffc31a
sanae aziz <aziz.sanae0101@gmail.com>
2022-12-25 19:42:29 +0100

    show students list using pages

--- GestionDesEtudiants/src/main/java/com/test/GestionDesEtudiants/service/EtudiantController.java
+++ GestionDesEtudiants/src/main/java/com/test/GestionDesEtudiants/service/EtudiantController.java
@@ -3,9 +3,12 @@ package com.test.GestionDesEtudiants.service;
 import com.test.GestionDesEtudiants.models.Etudiant;
 import com.test.GestionDesEtudiants.repository.EtudiantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,10 +19,13 @@ public class EtudiantController {
     private EtudiantRepository etudiantRepository;
 
     @RequestMapping(value="/Index")
-    public String Index(Model model){
-        List<Etudiant> etds = etudiantRepository.findAll();
-        model.addAttribute("etudiants", etds);
-
+    public String Index(Model model, @RequestParam(name = "page",defaultValue = "0")int p){
+        Page<Etudiant> pageEtudiants = etudiantRepository.findAll(PageRequest.of(  p,1));
+        model.addAttribute("pageEtudiants", pageEtudiants);
+        int pagescount =pageEtudiants.getTotalPages();
+        int [] pages=new int[pagescount];
+        for (int i=0;i<pagescount;i++) pages[i]=i;
+        model.addAttribute("pages",pages);
         return "etudiants";
     }
 
